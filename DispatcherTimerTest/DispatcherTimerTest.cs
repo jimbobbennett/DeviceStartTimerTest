4f829d981ec2f9801bab15e9995611cfeeff6277
Jim Bennett <jim@jimbobbennett.io>
2015-03-07 15:11:44 +0000

    Initial version

---
+++ DispatcherTimerTest/DispatcherTimerTest.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+using Xamarin.Forms;
+using System.Diagnostics;
+
+namespace DispatcherTimerTest
+{
+	public class App : Application
+	{
+		public App ()
+		{
+			// The root page of your application
+			MainPage = new ContentPage 
+			{
+				Content = new StackLayout 
+				{
+					VerticalOptions = LayoutOptions.Center,
+					Children = 
+					{
+						new Label 
+						{
+							XAlign = TextAlignment.Center,
+							Text = "Welcome to Xamarin Forms!"
+						}
+					}
+				}
+			};
+
+			Device.StartTimer(TimeSpan.FromSeconds(5), () =>
+				{
+					Debug.WriteLine("Timer tick");
+					return true;
+				});
+		}
+
+		protected override void OnStart ()
+		{
+			// Handle when your app starts
+		}
+
+		protected override void OnSleep ()
+		{
+			Debug.WriteLine("OnSleep");
+		}
+
+		protected override void OnResume ()
+		{
+			Debug.WriteLine("OnResume");
+		}
+	}
+}
+
